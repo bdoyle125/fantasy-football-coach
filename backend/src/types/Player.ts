f1c38d31a33fdd12cf3b9d2f3a899ff61e727a4c
Bryce Doyle <bdoyle125@gmail.com>
2025-11-11 08:44:29 -0600

    Remove injuryStatus
    That is a feature for the future

--- backend/src/types/Player.ts
+++ backend/src/types/Player.ts
@@ -6,7 +6,6 @@ class Player {
     team: string | null;
     position: string | null;
     stats: Stats; // You can define a more specific type based on the stats structure
-    injuryStatus?: string; // Optional injury status
 
     constructor(id: string, name: string, team: string | null, position: string | null, stats: Stats) {
         this.id = id;
