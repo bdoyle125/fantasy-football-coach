c985d4e9b3e82ff8dc226941a142aed0bf689f4c
Bryce Doyle <bdoyle125@gmail.com>
2025-11-19 22:21:29 -0600

    remove extra /

--- UI/src/service/TeamService.ts
+++ UI/src/service/TeamService.ts
@@ -5,7 +5,7 @@ export class TeamService {
     async fetchMyTeam(): Promise<Player[]> {
         const api = import.meta.env.VITE_API_URL || '';
         console.log('Using API URL:', api);
-        const response = await fetch(`${api}/api/myteam`, {
+        const response = await fetch(`${api}api/myteam`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
